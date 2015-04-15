a614b92c64ec2228c67b746222359c9fc773e9bd
yiban <yiban@yibandeiMac-2.local>
2015-04-15 09:16:16 +0800

    Initial Commit

---
+++ DoubanSameCity/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DoubanSameCity
+//
+//  Created by yiban on 15/4/15.
+//  Copyright (c) 2015年 yiban. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
